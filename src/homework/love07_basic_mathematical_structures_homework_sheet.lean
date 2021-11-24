aa5205f4a21f5cf5e64c772621c48d196f308dba
Wyatt Howe <wyatt_howe@brown.edu>
2021-11-24 11:23:05 -0500

    HW 7

--- src/homework/love07_basic_mathematical_structures_homework_sheet.lean
+++ src/homework/love07_basic_mathematical_structures_homework_sheet.lean
@@ -30,27 +30,77 @@ Use these tactics to solve the following problems.
 -/
 
 lemma num1 : 12345 < 67890 := 
-sorry
+by norm_num
 
 lemma num2 {α : Type} [linear_ordered_field α] : 123 + 45 < 67890/3 := 
-sorry 
+by norm_num
 
 lemma num3 (x : ℤ) (hx : (x : ℝ) < 25*100) : x < 25*100 := 
-sorry
+by exact_mod_cast hx
 
 /-!
 1.2 (1 point). For each of these statements, either prove it, or prove its negation. 
 Think about why the statement (or its negation) is true!
 -/
 
-lemma num4 : 7/3 > 2 := 
-sorry 
+lemma num4 : ¬(7/3 > 2) :=
+-- by norm_num
+begin
+  apply not_lt.mpr,
+  have h_int_div : (7/3 : ℕ) = (2 : ℕ) := by refl,
+  apply eq.ge, apply eq.symm,  -- case = of ≤ vs <
+  apply h_int_div,
+end
 
-lemma num5 : 40 - (2*30) + 20 = 0 := 
-sorry 
+lemma num5 : ¬( 40 - (2*30) + 20 = 0 ) := 
+-- by norm_num
+begin
+  intro hf,
+
+  have ht : 40 - (2*30) + 20 = 20 := begin
+    calc 40 - (2*30) + 20
+        = 40 - 60 + 20 : by norm_num
+    ... = 0 + 20 : by norm_num
+    ... = 20 : by refl,
+  end,
+
+  have hz : 0 = 20 := begin
+    have hi : 20 = 40 - (2*30) + 20 := begin
+      exact eq.symm ht,
+    end,
+    apply eq.symm,
+    rw hi,
+    apply hf,
+  end,
+  have hnz : 0 ≠ 20 := by linarith,
+
+  apply hnz,
+  apply hz,
+end
 
 lemma num6 : 5 / (2 - 1 - 1) + 8 = 2 * 4 := 
-sorry
+-- by norm_num
+begin
+  let lhs := 5 / (2 - 1 - 1) + 8,
+  have hlhs : lhs = 8 := by calc
+          5 / (2 - 1 - 1) + 8
+        = 5 / (1 - 1) + 8 : by norm_num
+    ... = 5 / 0 + 8 : by norm_num
+    ... = 0 + 8 : by norm_num
+    ... = 8 : by refl,
+
+  let rhs := 2 * 4,
+  have hrhs : rhs = 8 := by calc
+          2 * 4
+        = 8 : by refl,
+  
+  have h : lhs = rhs := begin
+    apply hlhs,
+    -- apply hrhs,
+  end,
+
+  apply h,
+end
 
 
 /-!
@@ -64,10 +114,32 @@ But I've made a mistake! What did I do wrong? Correct the statement of the theor
 You'll probably find the lemmas `nat.gcd_eq_gcd_ab` and `nat.coprime.gcd_eq_one` helpful.
 -/
 #check nat.gcd_eq_gcd_ab
-#check nat.coprime.gcd_eq_one 
+#check nat.coprime.gcd_eq_one
 
-lemma sum_eq_one_of_coprime (a b : ℕ) (h : nat.coprime a b) : ∃ u v, u*a+v*b = 1 :=
-sorry 
+-- Coefficients cannot both be natural numbers                         ↓
+lemma sum_eq_one_of_coprime (a b : ℕ) (h : nat.coprime a b) : ∃ (u v : ℤ), u*a + v*b = 1 :=
+begin
+  have bezout := nat.gcd_eq_gcd_ab,
+  have gcd_eq_one := nat.coprime.gcd_eq_one,
+
+  let u := nat.gcd_a a b,
+  let v := nat.gcd_b a b,
+  have hb : ↑(nat.gcd a b) = ↑a * u + ↑b * v := begin
+    apply bezout,
+  end,
+
+  have hgcd : ↑(nat.gcd a b) = 1 := begin
+    norm_cast,
+    apply gcd_eq_one h,
+  end,
+
+  have hq : ↑a * u + ↑b * v = 1 := by linarith,
+  have hi : u * ↑a + v * ↑b = 1 := by linarith,  -- commutivity
+
+  apply exists.intro u,
+  apply exists.intro v,
+  apply hi,
+end
 
 /-! ## Question 2: algebraic lemmas and classes (7 points)
 
@@ -77,6 +149,40 @@ and a right inverse `z`, then `y = z`.
 -/
 
 -- your answer here
+lemma add_monoid_trans_eq_inv {α : Type} [c : add_monoid α] :
+  -- ∀x, (∃y, (y + x = c.zero) → (∃z, (x + z = c.zero) → (y = z))) :=
+  -- ∀x, ∃y, ∃z, y+x=c.zero → x+z=c.zero → y=z :=
+  ∀x, ∀y, ∀z, y+x=c.zero → x+z=c.zero → y=z :=
+begin
+  intro x,
+  intros y z,
+  intro linv,
+  intro rinv,
+
+  have h : y + x = x + z := by simp [linv, rinv],
+
+  have hassoc : (y + x) + z = y + (x + z) := add_assoc y x z,
+
+  have hl : (y + x) + z = c.zero + z := by simp [linv],
+
+  have hr : y + (x + z) = y + c.zero := by simp [rinv],
+
+  have hlr : c.zero + z = y + c.zero := begin
+    apply (eq.congr hl hr).mp,
+    apply hassoc,
+  end,
+
+  have hly : c.zero + z = y := begin
+    simp [hlr],
+    exact add_monoid.add_zero y,
+  end,
+
+  have hlz : c.zero + z = z := begin
+    exact add_monoid.zero_add z,
+  end,
+
+  simp [hlz, ← hly],
+end
 
 
 /-!
@@ -94,18 +200,61 @@ bundle all of the necessary axioms into your definition.
 #check monolithic_group.group
 
 @[class] structure rng (α : Type) : Type :=
-sorry 
+
+-- (R, add) is an abelian group.
+(add          : α → α → α)
+(add_comm     : ∀a b, add a b = add b a)
+(add_assoc    : ∀a b c, add (add a b) c = add a (add b c))
+
+(zero         : α)
+(zero_add     : ∀a, add zero a = a)
+(add_zero     : ∀a, add a zero = a)
+
+(inv          : α → α)
+(add_inv      : ∀a, add (inv a) a = zero)
+
+-- (R, mul) is a semigroup.
+(mul          : α → α → α)
+(mul_assoc    : ∀a b c, mul (mul a b) c = mul a (mul b c))
+
+-- Multiplication distributes over addition.
+(dist_left    : ∀a b c, mul a (add b c) = add (mul a b) (mul a c))
+(dist_right   : ∀a b c, mul (add a b) c = add (mul a c) (mul b c))
 
 /-! 2.3 (2 points). Define an instance that shows that any `rng` is an `add_monoid`. -/
 
 instance {α : Type} [rng α] : add_monoid α :=
-sorry 
+begin
+  tactic.unfreeze_local_instances,
+  rename _inst_1 c,
+  exact {
+    -- Semigroup requirements
+    add := c.add,
+    add_assoc := c.add_assoc,
+
+    -- Additional monoid requirements
+    zero := c.zero,
+    zero_add := c.zero_add,
+    add_zero := c.add_zero,
+  },
+end
 
 /-! 2.4 (1 point). Show that for any `α : Type`, the type of functions `α → α` is 
 a monoid under the composition operator: `f * g = λ x, f (g x))` 
 -/
 
 instance (α : Type) : monoid (α → α) := 
-sorry
+begin
+  exact {
+    -- Semigroup requirements
+    mul := λf, λg, λx, f (g x),
+    mul_assoc := by norm_num,
+
+    -- Additional monoid requirements
+    one := λx, x,
+    one_mul := by norm_num,
+    mul_one := by norm_num,
+  },
+end
 
 end LoVe
